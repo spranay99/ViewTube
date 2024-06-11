4637877b6b2bb1eecd2ed91b81834dd969e855e0
test <pranayshah2206@gmail.com>
2024-06-11 12:26:57 +0530

    refactored code

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,32 +1,7 @@
 import React from "react";
-import { IoMdHome } from "react-icons/io";
-import { SiYoutubeshorts } from "react-icons/si";
-import { MdSubscriptions } from "react-icons/md";
-import { MdOutlineVideoLibrary } from "react-icons/md";
-import { GrHistory } from "react-icons/gr";
 import { useSelector } from "react-redux";
-
-import { HiOutlineFire } from "react-icons/hi2";
-import { RiShoppingBag4Line } from "react-icons/ri";
-import { PiMusicNote } from "react-icons/pi";
-import { BiMoviePlay } from "react-icons/bi";
-import { LuRadio } from "react-icons/lu";
-import { SiYoutubegaming } from "react-icons/si";
-import { ImNewspaper } from "react-icons/im";
-import { BsTrophy } from "react-icons/bs";
-import { AiOutlineBulb } from "react-icons/ai";
-import { TbHanger2 } from "react-icons/tb";
-import { MdOutlinePodcasts } from "react-icons/md";
-
-import { FaYoutube } from "react-icons/fa";
-import { SiYoutubemusic } from "react-icons/si";
-import { TbBrandYoutubeKids } from "react-icons/tb";
-
-import { IoSettingsOutline } from "react-icons/io5";
-import { MdOutlineFlag } from "react-icons/md";
-import { FiHelpCircle } from "react-icons/fi";
-import { BiCommentError } from "react-icons/bi";
 import { useLocation } from "react-router-dom";
+import { DefaultHome, Explore, Home, More, Others } from "../utils/icons";
 
 const SideBar = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
@@ -40,26 +15,17 @@ const SideBar = () => {
 
     return (
       <div className="hidden sm:block fixed dark:bg-[#0f0f0f] text-white p-2 h-[calc(100vh-56px)] ">
-        <div className="flex flex-col items-center gap-1 p-1 py-2 my-5 hover:bg-[#303030]/[0.6] hover:rounded-lg cursor-pointer">
-          <IoMdHome className="w-7 h-7" />
-          <span className="text-[12px]">Home</span>
-        </div>
-        <div className="flex flex-col items-center gap-1 p-1 my-5 hover:bg-[#303030]/[0.6] hover:rounded-lg cursor-pointer">
-          <SiYoutubeshorts className="w-6 h-6" />
-          <span className="text-[12px]">Shorts</span>
-        </div>
-        <div className="flex flex-col items-center gap-1 p-1 my-5 hover:bg-[#303030]/[0.6] hover:rounded-lg cursor-pointer">
-          <MdSubscriptions className="w-6 h-6" />
-          <span className="text-[12px]">Subscriptions</span>
-        </div>
-        <div className="flex flex-col items-center gap-1 p-1 my-5 hover:bg-[#303030]/[0.6] hover:rounded-lg cursor-pointer">
-          <MdOutlineVideoLibrary className="w-6 h-6" />
-          <span className="text-[12px]">You</span>
-        </div>
-        <div className="flex flex-col items-center gap-1 p-1 my-5 hover:bg-[#303030]/[0.6] hover:rounded-lg cursor-pointer">
-          <GrHistory className="w-5 h-6" />
-          <span className="text-[12px]">History</span>
-        </div>
+        {DefaultHome.map(({ icon, name }, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-col items-center gap-1 p-1 py-2 my-5 hover:bg-[#303030]/[0.6] hover:rounded-lg cursor-pointer"
+            >
+              {icon}
+              <span className="text-[12px]">{name}</span>
+            </div>
+          );
+        })}
       </div>
     );
   }
@@ -67,24 +33,17 @@ const SideBar = () => {
   return (
     <div className="fixed w-48 lg:w-60 dark:bg-[#0f0f0f] text-white flex flex-col h-[calc(100vh-56px)] overflow-y-scroll sidebar z-50">
       <ul className="w-full flex flex-col gap-1 p-2">
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <IoMdHome className="w-6 h-6" /> <span className="text-md">Home</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <SiYoutubeshorts className="w-5 h-5" />
-          <span className="text-md">Shorts</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <MdSubscriptions className="w-5 h-5" />
-          <span className="text-md">Subscriptions</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <MdOutlineVideoLibrary className="w-5 h-5" />
-          <span className="text-md">You</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <GrHistory /> <span className="text-md">History</span>
-        </li>
+        {Home.map(({ icon, name }, index) => {
+          return (
+            <li
+              key={index}
+              className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg"
+            >
+              {icon}
+              <span className="text-md">{name}</span>
+            </li>
+          );
+        })}
       </ul>
       <div className="my-2">
         <hr />
@@ -93,43 +52,17 @@ const SideBar = () => {
         <h1 className="md:font-bold ml-3 mb-2 md:text-lg text-md font-bold">
           Explore
         </h1>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <HiOutlineFire className="w-6 h-6" />
-          <span className="text-md">Trending</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <RiShoppingBag4Line className="w-5 h-5" />
-          <span className="text-md">Shopping</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <PiMusicNote className="w-5 h-5" />
-          <span className="text-md">Music</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <BiMoviePlay className="w-5 h-5" />
-          <span className="text-md">Movies</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <LuRadio /> <span className="text-md">Live</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <SiYoutubegaming /> <span className="text-md">Gaming</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <ImNewspaper /> <span className="text-md">News</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <BsTrophy /> <span className="text-md">Sports</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <AiOutlineBulb /> <span className="text-md">Courses</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <TbHanger2 /> <span className="text-md">Fashion & Beauty</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <MdOutlinePodcasts /> <span className="text-md">Podcasts</span>
-        </li>
+        {Explore.map(({ icon, name }, index) => {
+          return (
+            <li
+              key={index}
+              className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg"
+            >
+              {icon}
+              <span className="text-md">{name}</span>
+            </li>
+          );
+        })}
       </ul>
       <div className="my-2">
         <hr />
@@ -138,39 +71,32 @@ const SideBar = () => {
         <h1 className="md:font-bold ml-3 mb-2 md:text-lg text-md font-bold">
           More from ViewTube
         </h1>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <FaYoutube className="w-5 h-5 text-[#CD201F]" />
-          <span className="text-md">Youtube Premium</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <SiYoutubemusic className="w-5 h-5 text-[#CD201F]" />
-          <span className="text-md">Youtube Music</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <TbBrandYoutubeKids className="w-5 h-5 text-[#CD201F]" />
-          <span className="text-md">Music</span>
-        </li>
+        {More.map(({ icon, name }, index) => {
+          return (
+            <li
+              key={index}
+              className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg"
+            >
+              {icon}
+              <span className="text-md">{name}</span>
+            </li>
+          );
+        })}
       </ul>
       <div className="my-2">
         <hr />
       </div>
       <ul className="w-full flex flex-col gap-1 p-2">
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <IoSettingsOutline className="w-5 h-5" />
-          <span className="text-md">Settings</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <MdOutlineFlag className="w-5 h-5" />
-          <span className="text-md">Report History</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <FiHelpCircle className="w-5 h-5 " />
-          <span className="text-md">Help</span>
-        </li>
-        <li className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg">
-          <BiCommentError className="w-5 h-5" />
-          <span className="text-md">Send Feedback</span>
-        </li>
+        {Others.map(({ icon, name }, index) => {
+          return (
+            <li
+              key={index}
+              className="flex items-center gap-3 cursor-pointer hover:bg-[#303030]/[0.6] p-2 rounded-lg"
+            >
+              {icon} <span className="text-md">{name}</span>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
---
+++ src/utils/icons.jsx
@@ -0,0 +1,152 @@
+import { IoMdHome } from "react-icons/io";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdSubscriptions } from "react-icons/md";
+import { MdOutlineVideoLibrary } from "react-icons/md";
+import { RiHistoryFill } from "react-icons/ri";
+import { HiOutlineFire } from "react-icons/hi2";
+import { RiShoppingBag4Line } from "react-icons/ri";
+import { PiMusicNote } from "react-icons/pi";
+import { BiMoviePlay } from "react-icons/bi";
+import { LuRadio } from "react-icons/lu";
+import { SiYoutubegaming } from "react-icons/si";
+import { ImNewspaper } from "react-icons/im";
+import { BsTrophy } from "react-icons/bs";
+import { AiOutlineBulb } from "react-icons/ai";
+import { TbHanger2 } from "react-icons/tb";
+import { MdOutlinePodcasts } from "react-icons/md";
+
+import { FaYoutube } from "react-icons/fa";
+import { SiYoutubemusic } from "react-icons/si";
+import { TbBrandYoutubeKids } from "react-icons/tb";
+
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdOutlineFlag } from "react-icons/md";
+import { FiHelpCircle } from "react-icons/fi";
+import { BiCommentError } from "react-icons/bi";
+
+export const DefaultHome = [
+  {
+    icon: <IoMdHome fontSize={30} />,
+    name: "Home",
+  },
+  {
+    icon: <SiYoutubeshorts fontSize={25} />,
+    name: "Shorts",
+  },
+  {
+    icon: <MdSubscriptions fontSize={25} />,
+    name: "Subscriptions",
+  },
+  {
+    icon: <MdOutlineVideoLibrary fontSize={25} />,
+    name: "You",
+  },
+  {
+    icon: <RiHistoryFill fontSize={25} />,
+    name: "History",
+  },
+];
+
+export const Home = [
+  {
+    icon: <IoMdHome fontSize={25} />,
+    name: "Home",
+  },
+  {
+    icon: <SiYoutubeshorts fontSize={20} />,
+    name: "Shorts",
+  },
+  {
+    icon: <MdSubscriptions fontSize={20} />,
+    name: "Subscriptions",
+  },
+  {
+    icon: <MdOutlineVideoLibrary fontSize={20} />,
+    name: "You",
+  },
+  {
+    icon: <RiHistoryFill fontSize={20} />,
+    name: "History",
+  },
+];
+
+export const Explore = [
+  {
+    icon: <HiOutlineFire fontSize={20} />,
+    name: "Trending",
+  },
+  {
+    icon: <RiShoppingBag4Line fontSize={20} />,
+    name: "Shopping",
+  },
+  {
+    icon: <PiMusicNote fontSize={20} />,
+    name: "Music",
+  },
+  {
+    icon: <BiMoviePlay fontSize={20} />,
+    name: "Movies",
+  },
+  {
+    icon: <LuRadio fontSize={20} />,
+    name: "Live",
+  },
+  {
+    icon: <SiYoutubegaming fontSize={20} />,
+    name: "Gaming",
+  },
+  {
+    icon: <ImNewspaper fontSize={20} />,
+    name: "News",
+  },
+  {
+    icon: <BsTrophy fontSize={20} />,
+    name: "Sports",
+  },
+  {
+    icon: <AiOutlineBulb fontSize={20} />,
+    name: "Courses",
+  },
+  {
+    icon: <TbHanger2 fontSize={20} />,
+    name: "Fashion & Beauty",
+  },
+  {
+    icon: <MdOutlinePodcasts fontSize={20} />,
+    name: "Podcasts",
+  },
+];
+
+export const More = [
+  {
+    icon: <FaYoutube fontSize={25} className="text-[#CD201F]" />,
+    name: "Youtube Premium",
+  },
+  {
+    icon: <SiYoutubemusic fontSize={25} className=" text-[#CD201F]" />,
+    name: "Youtube Music",
+  },
+  {
+    icon: <TbBrandYoutubeKids fontSize={25} className=" text-[#CD201F]" />,
+    name: "Youtube Kids",
+  },
+];
+
+export const Others = [
+  {
+    icon: <IoSettingsOutline fontSize={20} />,
+    name: "Settings",
+  },
+  {
+    icon: <MdOutlineFlag fontSize={20} />,
+    name: "Report History",
+  },
+  {
+    icon: <FiHelpCircle fontSize={20} />,
+    name: "Help",
+  },
+  {
+    icon: <BiCommentError fontSize={20} />,
+    name: "Send Feedback",
+  },
+];
